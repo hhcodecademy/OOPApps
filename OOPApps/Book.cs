7b711664a1a05df028a181932a3e3977c8dd1b80
hhcodecademy <huseyn.hasanli@code.edu.az>
2024-06-15 13:47:56 +0400

    Add project files.

---
+++ OOPApps/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPApps
+{
+    internal class Book
+    {
+
+        public Book(string text)
+        {
+            this.Text = text;
+            string[] array = this.Text.Split(' ');
+            this._wordcount = array.Length;
+        }
+
+
+        private int _wordcount { get; set; }
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public int PageCount { get; set; }
+        public int WordCount
+        {
+
+            get
+            {
+                return _wordcount;
+            }
+
+        }
+
+
+        public int CalculateMemory()
+        {
+            int totalSize = this._wordcount * 2;
+            return totalSize;
+
+        }
+
+
+    }
+}
---
+++ OOPApps/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OOPApps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tempText = "A class can contain one or more constructors, fields, methods, properties, delegates, and events. They are called class members. A class and its members can have access modifiers such as public, private, protected, and internal, to restrict access from other parts of the program.\r\n\r\nLet's add different members to the Student class.";
+
+            Book bookH = new Book(tempText);
+       
+    
+            bookH.Title = "C# book";
+      
+            Console.WriteLine(bookH.PageCount);
+            Console.WriteLine(bookH.WordCount);
+        }
+    }
+}
